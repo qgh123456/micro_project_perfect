d19907f002c9c04f2185439c9b7f1a39bdfe63f7
qgh123456 <2104484868@qq.com>
2021-01-28 17:24:06 +0800

    修改服务调用

--- febs-server/febs-server-system/src/main/java/cc/mrbird/febs/server/system/config/FebsServerSystemResourceServerConfigure.java
+++ febs-server/febs-server-system/src/main/java/cc/mrbird/febs/server/system/config/FebsServerSystemResourceServerConfigure.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @ProjectName: micro_project_perfect
