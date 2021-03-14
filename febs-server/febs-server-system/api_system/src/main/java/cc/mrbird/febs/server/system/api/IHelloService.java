35385417ec1a5bdda4d52030ad413a8d880d9977
qgh123456 <2104484868@qq.com>
2021-03-14 22:19:29 +0800

    添加

--- febs-server/febs-server-system/api_system/src/main/java/cc/mrbird/febs/server/system/api/IHelloService.java
+++ febs-server/febs-server-system/api_system/src/main/java/cc/mrbird/febs/server/system/api/IHelloService.java
@@ -1,8 +1,10 @@
 package cc.mrbird.febs.server.system.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface IHelloService {
 
+    @GetMapping("hello")
     String hello(@RequestParam("name") String name);
 }
