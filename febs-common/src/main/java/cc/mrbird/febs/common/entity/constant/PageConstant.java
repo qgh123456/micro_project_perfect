a400bf298b95b98b031adbe76fbb53ad8487b541
qgh123456 <2104484868@qq.com>
2021-03-19 09:43:13 +0800

    添加

---
+++ febs-common/src/main/java/cc/mrbird/febs/common/entity/constant/PageConstant.java
@@ -0,0 +1,12 @@
+package cc.mrbird.febs.common.entity.constant;
+
+/**
+ * 分页常量
+ *
+ * @author MrBird
+ */
+public interface PageConstant {
+
+    String ROWS = "rows";
+    String TOTAL = "total";
+}
